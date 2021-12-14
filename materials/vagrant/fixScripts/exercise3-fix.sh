55f7db6fcd33c35944210e6f603386cc4e0a9022
Tolik1230 <tolik1230@gmail.com>
2021-12-14 10:50:10 +0200

    commit

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise3 here
+echo '<Location "/">' | sudo tee -a /etc/apache2/sites-available/000-default.conf
+echo 'Require all granted' | sudo tee -a /etc/apache2/sites-available/000-default.conf
+echo '</Location>' | sudo tee -a /etc/apache2/sites-available/000-default.conf
+sudo systemctl restart apache2
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo echo 'Host server1' >> /home/vagrant/.ssh/config
+echo '	HostName 192.168.100.10' | sudo tee -a /home/vagrant/.ssh/config
+echo '	UserKnownHostsFile /dev/null' | sudo tee -a /home/vagrant/.ssh/config
+echo '	PasswordAuthentication no' | sudo tee -a /home/vagrant/.ssh/config
+echo '	StrictHostKeyChecking no' | sudo tee -a /home/vagrant/.ssh/config
+echo '	IdentityFile /home/vagrant/.ssh/private_key1' | sudo tee -a /home/vagrant/.ssh/config
+echo '	IdentitiesOnly yes' | sudo tee -a /home/vagrant/.ssh/config
+echo 'cp /vagrant/.vagrant/machines/server1/virtualbox/private_key /home/vagrant/.ssh/private_key1' | sudo tee -a /home/vagrant/.bashrc
+echo 'chmod 700 /home/vagrant/.ssh/private_key1' | sudo tee -a /home/vagrant/.bashrc
