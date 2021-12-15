9e0b971c29e9605c6276c2c2504c70225eb190a3
Tolik1230 <95735420+Tolik1230@users.noreply.github.com>
2021-12-15 09:14:34 +0200

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-sudo echo 'Host server2' >> /home/vagrant/.ssh/config
-echo '	HostName 192.168.100.11' | sudo tee -a /home/vagrant/.ssh/config
-echo '	UserKnownHostsFile /dev/null' | sudo tee -a /home/vagrant/.ssh/config
-echo '	PasswordAuthentication no' | sudo tee -a /home/vagrant/.ssh/config
-echo '	StrictHostKeyChecking no' | sudo tee -a /home/vagrant/.ssh/config
-echo '	IdentityFile /home/vagrant/.ssh/private_key2' | sudo tee -a /home/vagrant/.ssh/config
-echo '	IdentitiesOnly yes' | sudo tee -a /home/vagrant/.ssh/config
-echo 'cp /vagrant/.vagrant/machines/server2/virtualbox/private_key /home/vagrant/.ssh/private_key2' | sudo tee -a /home/vagrant/.bashrc
-echo 'chmod 700 /home/vagrant/.ssh/private_key2' | sudo tee -a /home/vagrant/.bashrc
